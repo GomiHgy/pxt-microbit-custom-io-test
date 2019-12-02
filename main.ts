404848a804ec395d9d9ec9f21467cdb8d225612f
GomiHgy <h.furukawa1@gmail.com>
2019-12-02 14:34:09 +0900

    メソッド追加テスト

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
 namespace yinbitEx {
+    serial.redirect(
+        SerialPin.P8,
+        SerialPin.P2,
+        BaudRate.BaudRate115200
+    )
+
     //% blockId=show_strings block="コメント %v"
     export function noaction(text: string): void {
+    }
+
+    //% blockId=show_strings block="送る"
+    export function send(): void {
+        serial.writeString("FF,")
     }
 }
