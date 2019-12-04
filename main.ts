00a8733c8febe1f126cc9220655f911803fe83c8
GomiHgy <h.furukawa1@gmail.com>
2019-12-04 23:31:24 +0900

    HSV指定機能追加

--- main.ts
+++ main.ts
@@ -24,6 +24,49 @@ namespace yinbitEx {
         serial.writeString("02FFFF" + toHexString2(r) + toHexString2(g) + toHexString2(b) + "03\n")
     }
 
+    /**
+     * Converts a hue saturation luminosity value into a RGB color
+     * @param h hue from 0 to 360
+     * @param s saturation from 0 to 99
+     * @param l luminosity from 0 to 99
+     */
+    //% blockId=cmd_set_color_hsv_all 色相="全てのLEDを 色相 %h 度 彩度 %g ％ 明度 %b ％で点灯する"
+    export function setPixelColorHSV(h: number, s: number, l: number): void {
+        h = Math.round(h);
+        s = Math.round(s);
+        l = Math.round(l);
+
+        h = h % 360;
+        s = Math.clamp(0, 99, s);
+        l = Math.clamp(0, 99, l);
+        let c = Math.idiv((((100 - Math.abs(2 * l - 100)) * s) << 8), 10000); //chroma, [0,255]
+        let h1 = Math.idiv(h, 60);//[0,6]
+        let h2 = Math.idiv((h - h1 * 60) * 256, 60);//[0,255]
+        let temp = Math.abs((((h1 % 2) << 8) + h2) - 256);
+        let x = (c * (256 - (temp))) >> 8;//[0,255], second largest component of this color
+        let r$: number;
+        let g$: number;
+        let b$: number;
+        if (h1 == 0) {
+            r$ = c; g$ = x; b$ = 0;
+        } else if (h1 == 1) {
+            r$ = x; g$ = c; b$ = 0;
+        } else if (h1 == 2) {
+            r$ = 0; g$ = c; b$ = x;
+        } else if (h1 == 3) {
+            r$ = 0; g$ = x; b$ = c;
+        } else if (h1 == 4) {
+            r$ = x; g$ = 0; b$ = c;
+        } else if (h1 == 5) {
+            r$ = c; g$ = 0; b$ = x;
+        }
+        let m = Math.idiv((Math.idiv((l * 2 << 8), 100) - c), 2);
+        let r = r$ + m;
+        let g = g$ + m;
+        let b = b$ + m;
+        setPixelColorAll(r, g, b);
+    }
+
     function toHexString6(value: number): string {
         let list = [0, 0, 0, 0, 0, 0]
         let _16進数リスト = "0123456789ABCDEF"
